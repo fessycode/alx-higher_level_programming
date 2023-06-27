e0e4d03290264a7ed777ceb8e419fb5742897c7b
fessycode <fessydoris@gmail.com>
2023-06-27 13:33:43 -0700

    1-square file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Declaring class"""
+    def __init__(self, size):
+        """Setting up private instance"""
+        self.__size = size
