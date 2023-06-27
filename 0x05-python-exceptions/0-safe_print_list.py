f180f75727ba878da473c1d2f001a2ff074c0938
fessycode <fessydoris@gmail.com>
2023-06-26 18:33:29 -0700

    readme file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    for n  in range(x):
+        try:
+            print("{}".format(my_list[n]), end='')
+        except IndexError:
+            break
+        else:
+            count += 1
+
+    print()
+    return (count)
