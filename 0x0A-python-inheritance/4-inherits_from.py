74a25523616fe27c3c07139c9820d9f76cd9598f
fessycode <fessydoris@gmail.com>
2023-07-11 06:56:19 -0700

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Functions that checks is a class is a subclass
+of another class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if ``obj`` is a subclass
+    of ``a_class`` or False if otherwise
+    """
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
