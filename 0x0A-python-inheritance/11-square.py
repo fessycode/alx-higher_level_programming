f6693fb5d4330e2a61c96168ec63c4d6e65c9457
fessycode <fessydoris@gmail.com>
2023-07-11 07:24:11 -0700

    update

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Simple class extended from another class
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class Square being an extension or a
+    sub-class of Rectangle
+    """
+
+    def __init__(self, size):
+        """instantiation of the member of the class"""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        A string representation of the the Class
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        return self.__size ** 2
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Simple class that inherits from another
+class
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class, being a sub-class or a child-class
+    of BaseGeomertr
+    """
+
+    def __init__(self, width, height):
+        """
+        init method -- called on creation of new member
+        the class
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
