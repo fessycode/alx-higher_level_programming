03edc3425ed027d5928ed19fea0ae9bf321f4530
fessycode <fessydoris@gmail.com>
2023-09-12 14:31:02 -0700

    update

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const count = parseInt(process.argv[2]);
+const symbol = 'X';
+let square = '';
+let a;
+
+if (!count) {
+  console.log('Missing size');
+}
+
+for (a = 0; a < count; a++) {
+  square = square + symbol;
+}
+
+for (a = 0; a < count; a++) {
+  console.log(square);
+}
