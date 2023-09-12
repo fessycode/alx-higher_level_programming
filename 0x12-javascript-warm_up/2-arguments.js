c24f9f1ee16f2ab2d3ec29168aeeab60b703e081
fessycode <fessydoris@gmail.com>
2023-09-12 06:25:17 -0700

    update

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arg = process.argv.slice(2);
+if (arg.length < 1) {
+  console.log("No argument");
+}
+else if (arg.length === 1){
+  console.log("Argument found");
+}
+else{
+  console.log("Argument found");
+}
